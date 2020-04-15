9091f029a5c9080b8cb356e777ac026261f35f81
ShuaichenLau <68754807@qq.com>
2020-04-16 01:56:12 +0800

    Apollo搭建分布式配置中心

--- spring/boot230/src/main/java/com/silent/Application.java
+++ spring/boot230/src/main/java/com/silent/Application.java
@@ -4,6 +4,12 @@ package com.silent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 搭建Apollo 配置中心
+ * 什么是分布式配置中心
+ * @author liusc
+ * 2020-4-16 01:55:43
+ */
 @SpringBootApplication
 public class Application {
 
--- spring/boot230/src/main/java/com/silent/controller/IndexController.java
+++ spring/boot230/src/main/java/com/silent/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.silent.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,16 @@ import java.util.Date;
 @RestController
 public class IndexController {
 
+    @Value("${admin:default}")
+    private String name;
+
     @RequestMapping("/")
     public String index(){
         return new Date().toString();
+    }
+
+    @RequestMapping("/name")
+    public String getName(){
+        return name;
     }
 }
