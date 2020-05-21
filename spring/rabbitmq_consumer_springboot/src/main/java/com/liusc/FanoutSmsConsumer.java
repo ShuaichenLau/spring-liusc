97b342fa56766ef65ece34fe45089521505bb6cd
ShuaichenLau <68754807@qq.com>
2020-05-21 17:51:04 +0800

    删除乱码

--- spring/rabbitmq_consumer_springboot/src/main/java/com/liusc/FanoutSmsConsumer.java
+++ spring/rabbitmq_consumer_springboot/src/main/java/com/liusc/FanoutSmsConsumer.java
@@ -1,14 +1,12 @@
 package com.liusc;
 
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-//�ʼ�����
 @Component
 public class FanoutSmsConsumer {
 	@RabbitListener(queues = "fanout_sms_queue")
 	public void process(String msg) {
-		System.out.println("���������߻�ȡ��������Ϣmsg:" + msg);
+		System.out.println(msg);
 	}
 }
